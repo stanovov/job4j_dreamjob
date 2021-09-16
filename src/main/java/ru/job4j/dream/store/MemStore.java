a7b288c7aa0c6a5ea8c7b457d767efd1392803d0
Semyon Stanovov <sestanovov@gmail.com>
2021-09-16 22:25:59 +0300

    1. Написать тест на PostServlet. [#2518]

--- src/main/java/ru/job4j/dream/store/MemStore.java
+++ src/main/java/ru/job4j/dream/store/MemStore.java
@@ -27,58 +27,6 @@ public class MemStore implements Store {
     private final Map<Integer, User> users = new ConcurrentHashMap<>();
 
     private MemStore() {
-        String ls = System.lineSeparator();
-        posts.put(
-                1,
-                new Post(
-                        1,
-                        "Junior Java Job",
-                        "Наши ожидания:" + ls
-                                + "-глубокие теоретические познания Java;" + ls
-                                + "-обязательно наличие оконченных курсов по разработке на Java;" + ls
-                                + "-желателен опыт работы в области разработки на Java от полугода;" + ls
-                                + "-уровень английского Intermediate и выше.",
-                        LocalDateTime.of(2021, 9, 13, 19, 30, 0)
-                )
-        );
-        posts.put(
-                2,
-                new Post(
-                        2,
-                        "Middle Java Job",
-                        "Что мы ожидаем от вас:" + ls
-                                + " отличные знания Java 8-11, Spring Framework и Spring Boot и опыт их применения для "
-                                + " разработки распределенных систем;" + ls
-                                + " опыт работы c технологиями контейнеризации (Docker и т.п.) и с системами "
-                                + "оркестрации (Kubernetes, OpenShift и т.п.);" + ls
-                                + "опыт проектирования или участие в разработке высоконагруженных и отказоустойчивых "
-                                + "распределенных систем.",
-                        LocalDateTime.of(2021, 9, 12, 11, 0, 0)
-                )
-        );
-        posts.put(
-                3,
-                new Post(
-                        3,
-                        "Senior Java Job",
-                        "Мы занимаемся разработкой собственной инновационной платформы для "
-                                + "финтех-организаций и банков по всему миру. Наши заказчики из Европы, Африки и "
-                                + "Ближнего Востока запускают новые платежные сервисы, а мы помогаем им в этом от "
-                                + "стадии проектирования до запуска в эксплуатацию." + ls
-                                + ls
-                                + "От вас требуется:" + ls
-                                + "-Отличное знание Java 8/11+, опыт коммерческой разработки на Java от 4х лет." + ls
-                                + "-Опыт работы со Spring (DI, Security, MVC, Boot / Actuator), Hibernate, Maven / "
-                                + "Gradle, PostgreSQL / MySQL, Kafka / RabbitMQ или аналогами;" + ls
-                                + "-Хорошее знание базовых алгоритмов и структур данных, принципов ООП и функционального"
-                                + " программирования, навыки многопоточного программирования;" + ls
-                                + "-Умение самостоятельно проектировать приложения или отдельные сервисы;" + ls
-                                + "-Владение письменным английским языком.",
-                        LocalDateTime.of(2021, 8, 28, 15, 45, 25))
-        );
-        candidates.put(1, new Candidate(1, "Junior Java"));
-        candidates.put(2, new Candidate(2, "Middle Java"));
-        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static MemStore instOf() {
---
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.dream.servlet;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import ru.job4j.dream.model.Post;
+import ru.job4j.dream.store.MemStore;
+import ru.job4j.dream.store.PsqlStore;
+import ru.job4j.dream.store.Store;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PsqlStore.class)
+public class PostServletTest {
+    @Test
+    public void whenCreatePost() throws IOException, ServletException {
+        Store store = MemStore.instOf();
+
+        PowerMockito.mockStatic(PsqlStore.class);
+        PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
+
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+
+        PowerMockito.when(req.getParameter("id")).thenReturn("1");
+        PowerMockito.when(req.getParameter("name")).thenReturn("n");
+
+        new PostServlet().doPost(req, resp);
+
+        Post result = store.findPostById(1);
+        Assert.assertThat(result.getName(), is("n"));
+    }
+}
